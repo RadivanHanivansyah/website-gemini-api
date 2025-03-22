6999e4fcafc4e98b7178567b3dcf4fde06c54651
RadivanHanivansyah <radivan100@gmail.com>
2025-03-22 12:42:52 +0700

    menampilkan respone gemini api di web

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ const textBox = document.querySelector('#request');
 const text = document.querySelector('.question');
 const answer = document.querySelector('#response');
 button.addEventListener('click', () => {
+    text.classList.remove('hidden');
+    text.textContent = textBox.value;
     async function gemini() {
         const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBQ2_qNxfHE-z7B9XevxLcgIsf7EKj-k0I";
         const response = await fetch(url, {
             method: "POST",
             body: JSON.stringify({
                 "contents": [{
-                    "parts": [{ "text": "anime" }]
+                    "parts": [{ "text": textBox.value }]
                 }]
             }),
         });
-        const body = response.json();
-        console.log(body);
+        const body = await response.json();
+        const textResponse = body.candidates[0].content.parts[0].text;
+        answer.classList.replace('normal', 'response-text');
+        answer.textContent = textResponse;
     }
-    text.textContent = textBox.value
-    text.classList.remove('hidden');
     gemini();
 });
 
